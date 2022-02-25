40378607c770662ff7ec2f2eac0f3697a6ee23e4
Omar Faruk <omarfaruk.cse.fn@gmail.com>
2022-02-25 14:15:09 +0600

    falsy and truethy value fun

---
+++ true-false.js
@@ -0,0 +1,21 @@
+/* falsy value 
+1. false
+2. 0
+3. empty string
+4. undifine
+5. null
+6.NaN
+
+truthy value
+1. true 
+2. Any number positive or nagative
+3. Any string with white speach
+
+*/
+const x = false
+if (x) {
+    console.log('Variable is falsy')
+}
+else {
+    console.log('variable is truthi')
+}
