323b170ba1498b75e5cf97485feb490eaca51281
Omar Faruk <omarfaruk.cse.fn@gmail.com>
2022-02-25 13:09:08 +0600

    data type

---
+++ data-type.js
@@ -0,0 +1,6 @@
+const age = 19;
+console.log('type of age : ' + typeof age)
+const name = 'Abdullah al mamun'
+console.log('type of name: ' + typeof name)
+let a = false
+console.log('type of a   : ' + typeof a)
