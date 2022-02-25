2b26de92d3e22d875168f1a0243086ede22251ac
Omar Faruk <omarfaruk.cse.fn@gmail.com>
2022-02-25 13:59:58 +0600

    Primitive and non primitive data types

---
+++ primitive.js
@@ -0,0 +1,14 @@
+//fun factor frogramming p: p: don't take it seriously
+/* let a = 'hello'
+console.log(a)
+let b = a
+console.log(b)
+a = 'gelo'
+console.log(a, b) */
+
+
+const x = { job: 'web develiper' }
+const y = x
+console.log(x, y)
+x.job = 'front end developer'
+console.log(x, y)
